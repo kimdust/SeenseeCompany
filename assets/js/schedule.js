acefa89da2352f4379c0e113be9aeda98cc1acf6
kimdust <mji1223@naver.com>
2024-06-21 17:56:00 +0900

    schedule 2024

---
+++ assets/js/schedule.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $(".up_btn").hide();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $(".up_btn").fadeIn();
+      $(".up_btn").css("visibility", "visible");
+    } else {
+      $(".up_btn").fadeOut();
+      $(".up_btn").css("visibility", "hideen");
+    }
+  });
+
+  $(".up_btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+  $(
+    ".hd_gnb ul li:nth-child(1) a, .hd_gnb ul li:nth-child(2) a, .hd_gnb ul li:nth-child(4) a"
+  ).mouseenter(function () {
+    $(this).css("font-family", "GmarketSansBold");
+  });
+
+  $(
+    ".hd_gnb ul li:nth-child(1) a, .hd_gnb ul li:nth-child(2) a, .hd_gnb ul li:nth-child(4) a"
+  ).mouseleave(function () {
+    $(this).css("font-family", "GmarketSansMedium");
+  });
+});
